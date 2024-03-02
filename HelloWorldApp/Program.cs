9b7458d05b52db5960879581275e0ecdb70101cd
John Dutchover <johndutchover@users.noreply.github.com>
2024-03-02 12:49:43 -0500

    Initial commit

---
+++ HelloWorldApp/Program.cs
@@ -0,0 +1,20 @@
+﻿// This is a simple C# program that prints "Hello World" to the console.
+
+// Declare a namespace named HelloWorldApp
+namespace HelloWorldApp
+{
+    // Import the System namespace, which contains the Console class
+    using System;
+
+    // Declare a class named Program
+    class Program
+    {
+        // Declare the entry point of the program - the Main method
+        static void Main()
+        {
+            // Print "Hello, World!" to the console
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+
